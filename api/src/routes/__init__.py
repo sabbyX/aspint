b6946e6d4891c2fad29dd8ab59fb917019a036f0
sabeeh <sabby.6@outlook.com>
2024-09-23 21:32:57 +0530

    more proxies and healthcheck

--- api/src/routes/__init__.py
+++ api/src/routes/__init__.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter
 from .instance_control import router as instance_router
 from .config import router as config_router
 from .internal import router as internal_router
+from .health import router as health_router
 
 main_router = APIRouter()
 
 main_router.include_router(instance_router)
 main_router.include_router(config_router)
 main_router.include_router(internal_router)
+main_router.include_router(health_router)
 
 
 @main_router.get('/')
