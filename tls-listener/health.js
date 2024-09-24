752da1fb63400e615b8aa53bbf7415081bbbd060
sabeeh <sabby.6@outlook.com>
2024-09-24 11:46:18 +0530

    improv tls-adv-listener

---
+++ tls-listener/health.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const HEALTH_API_ENDPOINT = "http://backend:8000/health/setWorkerHealth/";
+const WORKER_TYPE = process.env.WORKER_TYPE;
+// spec:
+// 2 WORKER_TYPE (s)
+// - ASSISTIVE
+// - INDEPENDENT
+const WORKER_ID = process.env.WORKER_ID;
+
+// 200 OK
+// 102 RESTARTING
+// 403 FORBIDDEN -> BLOCKED BY WAF
+// 408 TIMEOUT
+// 500 OTHER ERRORS
+export async function setHealthInfo(center, code) {
+    await axios.post(
+        HEALTH_API_ENDPOINT,
+        {
+            "center": center,
+            "health_code": code,
+            "worker_type": WORKER_TYPE,
+            "worker_id": WORKER_ID, 
+        }
+    )
+}
--- tls-listener/link.js
+++ tls-listener/link.js
@@ -31,6 +31,22 @@ export function cfHopRq(c) {
     }
 }
 
+export function cfHopRq2(c) {
+    switch (c) {
+        case "be":
+            return `https://visas-be.tlscontact.com/`
+        case "ch":
+            return `https://visas-ch.tlscontact.com/`
+        case "de":
+            return `https://visas-de.tlscontact.com/`
+        case "fr":
+            return `https://fr.tlscontact.com/`
+        default:
+            console.log("Unexpected country: ", c)
+            return ""
+    }
+}
+
 export function apPage(c) {
     switch (c) {
         case "be":
