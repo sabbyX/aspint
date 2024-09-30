5f0ab393390f252a995d477430a8dcd49c357127
sabeeh <sabby.6@outlook.com>
2024-09-30 22:39:44 +0530

    update

--- tls-listener/constants.js
+++ tls-listener/constants.js
@@ -5,15 +5,17 @@ export const DEF_PROXY = process.env.PROXY;
 export const DEF_PROXY_PORT = process.env.PROXY_PORT
 export const DEF_PROXY_UN = process.env.PROXY_UN
 export const DEF_PROXY_PW = process.env.PROXY_PW
-export const BROWSER_PATH = process.env.BROWSER_PATH
+export const BROWSER_PATH = process.env.BROWSER_PATH ? process.env.BROWSER_PATH : "/usr/bin/brave-browser";
 export const BACKEND_HOST = "http://backend:8000";
 
 // dev
 
-// export const WORKER_TYPE = "ASSISTIVE";
-// export const WORKER_ID = "dev-1";
-// export const LISTENERS = "gbLON2fr-1";
-// export const DEF_PROXY = "118.193.59.102";
-// export const DEF_PROXY_PORT = 2334
-// export const DEF_PROXY_UN = "u197aa4f6566105bb-zone-custom-region-gb-asn-AS12576"
-// export const DEF_PROXY_PW = "u197aa4f6566105bb"
+//  export const WORKER_TYPE = "ASSISTIVE";
+//  export const WORKER_ID = "dev-1";
+//  export const LISTENERS = "gbLON2de";
+//  export const DEF_PROXY = "localhost"
+//  export const DEF_PROXY_PORT = 7777
+//  export const DEF_PROXY_UN = undefined
+//  export const DEF_PROXY_PW = undefined
+//  export const BROWSER_PATH = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+//  export const BACKEND_HOST = "http://localhost:8000";
