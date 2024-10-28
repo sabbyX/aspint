2a9371c4aba75593e5e46f4c213d86e44e65695f
sabeeh <sabby.6@outlook.com>
2024-10-28 22:14:58 +0530

    api-rs: improve view instances sse

--- api-rs/src/routes/service/view_instance_sse.rs
+++ api-rs/src/routes/service/view_instance_sse.rs
@@ -6,22 +6,62 @@ use axum::response::Sse;
 use axum::response::sse::Event;
 use tokio_stream::StreamExt as _;
 use futures::stream::{Stream};
-use mongodb::bson::{bson, doc};
+use indexmap::IndexSet;
+use mongodb::bson::{bson, doc, Document};
 use mongodb::change_stream::event::OperationType;
 use mongodb::Database;
 use mongodb::options::{FullDocumentBeforeChangeType, FullDocumentType};
 use serde_json::json;
 use crate::model::ab_application::ABApplication;
 use crate::model::ab_health::ABHealth;
+use crate::model::ab_server::ABServer;
 use crate::server_err::ServerError;
 use crate::state::AppState;
 
 fn internal_stream(db: Database) -> impl Stream<Item = Result<Event, anyhow::Error>> {
+
     stream! {
-        let docs: Vec<ABApplication> = db.collection::<ABApplication>("autobook_applications").find(doc! {}).await?.collect().await?;
+        let init_applications: Vec<Document> = db.collection::<ABApplication>("autobook_applications")
+            .aggregate(vec![
+                doc! {
+                    "$lookup": {
+                        "from": "autobook_logs",
+                        "localField": "formid",
+                        "foreignField": "formid",
+                        "pipeline": vec! [
+                            doc! {"$addFields": { "latestStatus": { "$last": "$logs" }}},
+                        ],
+                        "as": "status_logs",
+                    }
+                },
+                doc! {
+                    "$project": {
+                        "_id": 0,
+                        "formid": 1,
+                        "name": 1,
+                        "email": 1,
+                        "issuer": 1,
+                        "country": 1,
+                        "status": { "$arrayElemAt": ["$status_logs.latestStatus", -1] },
+                    }
+                },
+            ])
+            .await?
+            .collect()
+            .await?;
         let event = Event::default()
             .event("initApplications")
-            .json_data(json!({"data": docs}))?;
+            .json_data(json!({"data": init_applications}))?;
+        yield Ok(event);
+        
+        let init_abservers: Vec<ABServer> = db.collection::<ABServer>("autobook_servers").find(doc! {})
+            .projection(doc! { "_id": 0})
+            .await?
+            .collect()
+            .await?;
+        let event = Event::default()
+            .event("initABServers")
+            .json_data(json!({"data": init_abservers}))?;
         yield Ok(event);
 
         let mut db_change_stream = db.watch()
@@ -68,6 +108,31 @@ fn internal_stream(db: Database) -> impl Stream<Item = Result<Event, anyhow::Err
                                 _ => Event::default().event("IGNORE_EVENT"),
                             }
                         },
+                        "autobook_servers" => {
+                            match event.operation_type {
+                                OperationType::Insert | OperationType::Update => {
+                                    let curr_doc: ABServer = serde_json::from_str(&serde_json::to_string(&event.full_document.unwrap())?)?;
+                                    if let Some(generic_before_doc) = event.full_document_before_change {
+                                        let before_doc: ABServer = serde_json::from_str(&serde_json::to_string(&generic_before_doc)?)?;
+                                        if curr_doc.queue.len() > before_doc.queue.len() {
+                                            Event::default()
+                                                .event("newABTask")
+                                                .json_data(json!({"server_id": curr_doc.server, "addition": curr_doc.queue.last()}))?
+                                        } else {
+                                            let curr_set: IndexSet<&i32> = IndexSet::from_iter(curr_doc.queue.iter());
+                                            let before_set: IndexSet<&i32> = IndexSet::from_iter(before_doc.queue.iter());
+                                            let deletion = before_set.difference(&curr_set).collect::<Vec<_>>();
+                                            Event::default().event("removeABTask")
+                                                .json_data(json!({"server_id": curr_doc.server, "deletion": deletion.last()}))?
+                                        }
+                                    } else {
+                                        Event::default().event("newABTask")
+                                            .json_data(json!({"server_id": curr_doc.server, "addition": curr_doc.queue.last()}))?
+                                    }
+                                },
+                                _ => Event::default().event("IGNORE_EVENT"),
+                            }
+                        }
                         _ => todo!()
                     };
                     yield Ok(event);
@@ -80,25 +145,14 @@ fn internal_stream(db: Database) -> impl Stream<Item = Result<Event, anyhow::Err
 pub async fn view_instances_sse(
     State(state): State<Arc<AppState>>,
 ) -> Result<Sse<impl Stream<Item = Result<Event, anyhow::Error>>>, ServerError> {
-    let _ = state.db
-        .database("aspint")
-        .run_command(doc! {
-            "collMod": "autobook_applications",
-            "changeStreamPreAndPostImages": {
-                "enabled": true,
-            }
-        }).await;
-    let _ = state.db
-        .database("aspint")
-        .run_command(doc! {
-            "collMod": "autobook_logs",
-            "changeStreamPreAndPostImages": {
-                "enabled": true,
-            }
-        }).await;
-    Ok(Sse::new(internal_stream(state.db.database("aspint"))).keep_alive(
-        axum::response::sse::KeepAlive::new()
-            .interval(Duration::from_secs(1))
-            .text("keep-alive-text"),
-    ))
+    for i in ["autobook_applications", "autobook_logs", "autobook_servers"] {
+        let _ = state.db.database("aspint").run_command(doc! {"collMod": i,"changeStreamPreAndPostImages": {"enabled": true,}}).await;
+    }
+    Ok(Sse::new(internal_stream(state.db.database("aspint")))
+        .keep_alive(
+            axum::response::sse::KeepAlive::new()
+                .interval(Duration::from_secs(1))
+                .text("keep-alive-text"),
+        )
+    )
 }
